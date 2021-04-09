e669dbc825bf7e9574a0dab1e46c9f2088790d79
Rostyslav Sipakov <ross.s@missioniz.com>
2021-04-09 05:32:58 -0400

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -148,18 +148,16 @@ export const Header = (): JSX.Element => {
                                         className="text-base font-medium text-gray-900 hover:text-gray-700">
                                         Нормативи
                                     </a>
-
                                     <a
                                         href="/#"
                                         className="text-base font-medium text-gray-900 hover:text-gray-700">
                                         Інновації
                                     </a>
-
-                                    <a
-                                        href="/#"
-                                        className="text-base font-medium text-gray-900 hover:text-gray-700">
-                                        Контакти
-                                    </a>
+                                    <Link href={'/contact'}>
+                                        <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                            Контакти
+                                        </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
