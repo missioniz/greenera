3c634cb5b2f3b9ec7eb4e4cf5f05bd7a107efa22
Rostyslav Sipakov <ross.s@missioniz.com>
2021-04-09 05:02:27 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import useTranslation from 'next-translate/useTranslation';
-
 import Layout from '../components/layout';
 import HomeHero from '../sections/homeHero';
 import HomeIntro from '../sections/homeIntro';
@@ -10,11 +9,10 @@ export const Home = (): JSX.Element => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { t } = useTranslation();
     return (
-        <Layout contact>
+        <Layout>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <HomeHero />
             <HomeIntro />
         </Layout>
     );
