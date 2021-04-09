952b523ec44ec60671c1f7e31bbfd901bea211e6
Rostyslav Sipakov <ross.s@missioniz.com>
2021-04-09 05:06:23 -0400

    Create test.tsx

---
+++ src/pages/test.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import useTranslation from 'next-translate/useTranslation';
+
+import Layout from '../components/layout';
+import HomeHero from '../sections/homeHero';
+import HomeIntro from '../sections/homeIntro';
+export const siteTitle = 'GreenEra | Test';
+
+export const Test = (): JSX.Element => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { t } = useTranslation();
+    return (
+        <Layout>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <HomeHero />
+            <HomeIntro />
+        </Layout>
+    );
+};
+
+export default Test;
