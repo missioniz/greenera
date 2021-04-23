d3799179ca29ea467c6aa82c5719e3b497c6946f
Rostyslav <ross.s@missioniz.com>
2021-04-23 19:51:08 -0400

    added Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globalStyles/global.css';
 
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import * as ga from '../lib/ga';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            ga.pageview(url);
+        };
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
     return <Component {...pageProps} />;
 };
 
